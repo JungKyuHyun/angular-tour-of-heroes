9a808edf860928a6c907dc0203175d87ff948ac5
정규현 (Jacob) <kh.jung@ecubelabs.com>
2019-10-02 11:51:36 +0900

    Fix HTTP logic2
    하다가 너무 졸림... 조금씩 감이 오는 중

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,27 +1,69 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
+import { ThrowStmt } from '@angular/compiler';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient, // HttpClient가 반환하는 모든 것은 Observable로 반환한다.
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: 메시지는 히어로 데이터를 가져온 후에 보내야 합니다
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroUrl)
+    .pipe(
+      tap( _ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+// GET: id에 해당하는 히어로 데이터 가져오기. 존재하지 않으면 404를 반환
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
-  getHero(id: number): Observable<Hero> {
-    // TODO: 이 메시지는 서버에서 히어로 정보를 가져온 후에 보내야 합니다.
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
+
+  // PUT: 서버에 저장된 히어로 데이터를 변경
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroUrl,hero, httpOptions).pipe(
+      tap(_ => this.log('updated hero id=$hero id')),
+      catchError(this.catchError(this.handle<any>(this.updateHero)))
+    );
+  }
+
+  /**
+   * Http 요청이 실패한 경우 처리
+   * 애플리케이션 로직 흐름은 그대로 유지
+   * @param 기본값으로 반환할 객체
+   * @param operation : 실패한 동작의 이름
+   * @param result : 기본값으로 반환할 객체
+   */
+
+   private handleError<T>(operation = 'operation', result?: T) {
+     return (error: any): Observable<T> => {
+
+      // TODO: 리모트 서버로 에러 메시지 보내기
+      console.error(error);
+
+      // TODO: 사용자가 이해할 수 있는 형태로 변환하기
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 애플리케이션 로직이 끊기지 않도록 기본값으로 받은 객체를 반환
+      return of(result as T);
+    };
+   }
 }
